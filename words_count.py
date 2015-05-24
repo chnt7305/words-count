8449ccbb14ab67ecccc4a4cb51161217074aea07
yun <chnt7305@gmail.com>
2015-05-24 12:15:38 +0800

    Update words_count.py

--- words_count.py
+++ words_count.py
@@ -7,9 +7,14 @@ file_one = raw_input("请输入文件路径：")		#导入要处理的文件
 
 with open(file_one,"r") as f:					#打开文件
 	file=f.read()
-	words_list = re.findall(r"[a-zA-Z]+",file)		#提取单词
+	words_list0 = re.findall(r"[a-zA-Z]+",file)		#提取单词
 	
-words_dict={}									#建立一个空的字典
+words_dict={}							#建立一个空的字典
+words_list=[]							#建立一个空列表
+
+for ii in words_list0:						#将单词都改成小写
+	ii = ii.lower()
+	words_list.append(ii)
 
 for i in words_list:							#统计单词的频率
 	if words_list.count(i)>0:					#将单词作为Key,出现频率作为value建立一个字典
